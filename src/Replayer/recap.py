17bc68ac838c90e13f0aa67448010a2d8797ae68
dovakiin0 <subedibs@outlook.com>
2021-03-13 13:43:54 +0545

    replayer done

--- src/Replayer/recap.py
+++ src/Replayer/recap.py
@@ -2,35 +2,58 @@ import configparser
 import pickle
 from rlbot.utils.game_state_util import BallState, GameState, CarState, Rotator, Physics, Vector3
 import time
+import keyboard
 
 
 class Recap:
     def __init__(self):
         self.file = None
-        self.start_time = 10
+        self.start_time = 0
+        self.pack_start = None
+        self.replay_start = 0
+        self.packi = None
+        self.index = 0
+        self.load = False
 
     def init(self):
         config = configparser.ConfigParser()
         config.read("src/config.ini")
         self.file = config["REPLAYER"]["file"]
         self.start_time = float(config["REPLAYER"]["start_time"])
+        self.load = False
 
     def replayer(self, set_game_state):
-        with open("src/Snapshot/dumper/snapshot#3.pickle", "rb") as testi:
-            packi = pickle.load(testi)
-        # starting_state = GameState.create_from_gametickpacket(
-        #     packi[0])
-        # carsta0 = CarState(
-        #     physics=starting_state.cars[1].physics, boost_amount=starting_state.boosts)
-        # carsta1 = CarState(
-        #     physics=starting_state.cars[2].physics, boost_amount=starting_state.boosts)
-        # carsta2 = CarState(
-        #     physics=starting_state.cars[3].physics, boost_amount=starting_state.boosts)
-        # starting_gamestate = GameState(ball=starting_state.ball, cars={
-        #     0: carsta0, 1: carsta1, 2: carsta2})
-        # self.set_game_state(starting_gamestate)
+        if not self.load:
+            with open(f"src/Snapshot/dumper/{self.file}", "rb") as testi:
+                self.packi = pickle.load(testi)
+            self.pack_start = self.packi[0].game_info.seconds_elapsed
+            self.replay_start = self.pack_start + self.start_time
+            for ind, paks in enumerate(self.packi):
+                if float("%.1f" % paks.game_info.seconds_elapsed) == float("%.1f" % self.replay_start):
+                    self.index = ind
+            self.load = True
 
-        for pic in packi:
+        starting_state = GameState.create_from_gametickpacket(
+            self.packi[self.index])
+        carsta0 = CarState(
+            physics=starting_state.cars[0].physics, boost_amount=starting_state.boosts)
+        carsta1 = CarState(
+            physics=starting_state.cars[1].physics, boost_amount=starting_state.boosts)
+        carsta2 = CarState(
+            physics=starting_state.cars[2].physics, boost_amount=starting_state.boosts)
+        carsta3 = CarState(
+            physics=starting_state.cars[3].physics, boost_amount=starting_state.boosts)
+        carsta4 = CarState(
+            physics=starting_state.cars[4].physics, boost_amount=starting_state.boosts)
+        carsta5 = CarState(
+            physics=starting_state.cars[5].physics, boost_amount=starting_state.boosts)
+        starting_gamestate = GameState(ball=starting_state.ball, cars={
+            0: carsta0, 1: carsta1, 2: carsta2, 3: carsta3, 4: carsta4, 5: carsta5})
+        set_game_state(starting_gamestate)
+
+        for pic in self.packi[self.index:]:
+            if keyboard.is_pressed("+"):
+                break
             state = GameState.create_from_gametickpacket(pic)
             carsta0 = CarState(
                 physics=state.cars[0].physics)
--- src/replayer.py
+++ src/replayer.py
@@ -2,16 +2,14 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 import keyboard
 from Replayer.recap import Recap
-from rlbot.utils.game_state_util import BallState, GameState, CarState, Rotator, Physics, Vector3
-import time
-import pickle
 
 
 class MyBot(BaseAgent):
     def __init__(self, name, team, index):
         super().__init__(name, team, index)
         self.replayer = None
-        self.toggle_replay = False
+        # self.counter = 3
+        # self.load = False
 
     def initialize_agent(self):
         self.replayer = Recap()
@@ -24,13 +22,17 @@ class MyBot(BaseAgent):
         controls = SimpleControllerState()
 
         if keyboard.is_pressed("-"):
-            if self.toggle_replay:
-                self.toggle_replay = False
-            else:
-                self.toggle_replay = True
-
-        if self.toggle_replay:
+            # self.start = False
+            # self.counter = 3
+            # self.renderer.begin_rendering()
             self.replayer.init()
+            # self.renderer.draw_string_2d(
+            #     20, 500, 2, 2, count_text, self.renderer.white())
+            # self.counter -= 1
+            # if self.counter == 0:
+            #     self.start = True
+            # self.renderer.end_rendering()
+            # if self.start:
             self.replayer.replayer(self.set_game_state)
 
         return controls
